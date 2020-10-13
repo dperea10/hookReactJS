7fc88a778201a9307408792d766ebda5649046a4
Diego Perea <diego.perea.dp@gmail.com>
2020-10-12 20:41:42 -0500

    feat(useeffect): implements useEffect

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from '@material-ui/core';
 import queryString from 'query-string';
 
+import { searchMovie } from '../../redux/action/search';
+
 export default ({ location }) => {
 
-    console.log(location);
+    useEffect(() => {
+      const { movieName } = queryString.parse(location.search);
+     console.log(movieName);
+    });
+
     return(
         <Container>
             Results
